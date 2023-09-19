d5d0b5bb5a039513f435ebb9774c7e312d5ae460
joslinarevalo <ra16025@ues.edu.sv>
2023-09-18 23:49:25 -0600

    Agregando boton de busqueda en las card de patogenos

--- src/app/pages/patogenos/pages/mostrar/mostrar.component.ts
+++ src/app/pages/patogenos/pages/mostrar/mostrar.component.ts
@@ -3,6 +3,7 @@ import { ITipoCausa } from "../../../causa-enfermedad/models/TipoCausa";
 import { CausaenfermedadService } from "../../../causa-enfermedad/services/causaenfermedad.service";
 import { Router } from "@angular/router";
 import { DomSanitizer } from "@angular/platform-browser";
+
 @Component({
   selector: "app-mostrar",
   templateUrl: "./mostrar.component.html",
@@ -10,7 +11,10 @@ import { DomSanitizer } from "@angular/platform-browser";
 })
 export class MostrarComponent implements OnInit {
   @Input() alltipocausa!: ITipoCausa[];
+  searchTerm: string = "";
+  filteredCausas: ITipoCausa[] = []; 
   imagen: any;
+
   constructor(
     private causaenfermedad: CausaenfermedadService,
     private router: Router,
@@ -20,6 +24,19 @@ export class MostrarComponent implements OnInit {
   ngOnInit() {
     this.listatipo();
   }
+
+  filterCards() {
+    // Si el término de búsqueda está vacío, muestra todas las cards
+    if (this.searchTerm.trim() === "") {
+      this.filteredCausas = this.alltipocausa;
+    } else {
+      // Filtrar el array de datos usando el término de búsqueda
+      this.filteredCausas = this.alltipocausa.filter((causa) =>
+        causa.tipoTC.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+  }
+
   listatipo() {
     this.causaenfermedad.listaDeTipoCausa().subscribe((resp) => {
       this.alltipocausa = resp;
@@ -31,8 +48,12 @@ export class MostrarComponent implements OnInit {
           element.imagen = this.imagen;
         });
       });
+
+      // Inicializa filteredCausas después de cargar los datos en alltipocausa
+      this.filteredCausas = this.alltipocausa;
     });
   }
+
   ObtenerImagen(url: string) {
     this.causaenfermedad.getImagen(url).subscribe((resp) => {
       console.log(resp);
@@ -46,6 +67,4 @@ export class MostrarComponent implements OnInit {
     // Navega a la ruta del componente de detalle, pasando el ID como parámetro
     this.router.navigate(['patogenos/detalle', idTipoCausa]);
   }
- 
-  
 }
--- src/app/pages/patogenos/patogenos.module.ts
+++ src/app/pages/patogenos/patogenos.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from "@angular/common";
 import { PatogenosRoutingModule } from "./patogenos-routing.module";
 import { MostrarComponent } from "./pages/mostrar/mostrar.component";
 import { DetalleComponent } from "./pages/detalle/detalle.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     MostrarComponent,
     DetalleComponent
   ],
-  imports: [CommonModule, PatogenosRoutingModule],
+  imports: [CommonModule, PatogenosRoutingModule,FormsModule],
 })
 export class PatogenosModule {}
